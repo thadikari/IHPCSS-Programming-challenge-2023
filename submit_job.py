7f6e3d52491b44e56b4ce8dbbbff7726e591dd49
Tharindu Adikari <tadikari@br014.ib.bridges2.psc.edu>
2023-07-13 10:03:05 -0400

    slurm script changes

--- submit_job.py
+++ submit_job.py
@@ -10,22 +10,22 @@ for name in *.txt; do sed '4q;d' $name; echo "       $name"; done
 
 
 def main():
-    for OMP_NUM_THREADS in _a.OMP_NUM_THREADS:
+    for NUM_NODES in _a.NUM_NODES:
         for MPI_PROCESS_COUNT in _a.MPI_PROCESS_COUNT:
-            for NUM_GPU in _a.NUM_GPU:
-                run(OMP_NUM_THREADS, MPI_PROCESS_COUNT, NUM_GPU)
+            for OMP_NUM_THREADS in _a.OMP_NUM_THREADS:
+                for NUM_GPU in _a.NUM_GPU:
+                    run(OMP_NUM_THREADS, MPI_PROCESS_COUNT, NUM_GPU, NUM_NODES)
 
 
-def run(OMP_NUM_THREADS, MPI_PROCESS_COUNT, NUM_GPU):
+def run(OMP_NUM_THREADS, MPI_PROCESS_COUNT, NUM_GPU, NUM_NODES):
 
     dev = 'cpu' if _a.cpu else 'gpu'
     base_name = f'submit_{dev}_node.slurm'
-    with open(base_name, 'r') as fp:
-        cc = fp.read()
+    with open(base_name, 'r') as fp: cc = fp.read()
 
     runs_dir = os.path.expanduser('~') + '/runs'
     Path(f'{runs_dir}/outputs').mkdir(parents=True, exist_ok=True)
-    prefix = f'{OMP_NUM_THREADS}_{MPI_PROCESS_COUNT}_{NUM_GPU}_{dev}'
+    prefix = f'{NUM_NODES}_{MPI_PROCESS_COUNT}_{OMP_NUM_THREADS}_{NUM_GPU}_{dev}'
 
     if OMP_NUM_THREADS:
         new = f'export OMP_NUM_THREADS={OMP_NUM_THREADS};'
@@ -40,6 +40,11 @@ def run(OMP_NUM_THREADS, MPI_PROCESS_COUNT, NUM_GPU):
     if NUM_GPU:
         cc = cc.replace('#SBATCH --gres=gpu:v100-16:1', f'#SBATCH --gres=gpu:v100-16:{NUM_GPU}')
 
+    if NUM_NODES:
+        new = f'#SBATCH -N {NUM_NODES}'
+        cc = cc.replace('#SBATCH -N 2', new)
+        cc = cc.replace('#SBATCH -N 1', new)
+
     new_name = f'{prefix}_{base_name}'
     new_path = f'{runs_dir}/{new_name}'
     with open(new_path, 'w') as fp: fp.write(cc)
@@ -51,9 +56,10 @@ def run(OMP_NUM_THREADS, MPI_PROCESS_COUNT, NUM_GPU):
 
 def make_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--NUM_GPU', type=int, nargs='+', default=[None])
+    parser.add_argument('--NUM_NODES', type=int, nargs='+', default=[None])
     parser.add_argument('--OMP_NUM_THREADS', type=int, nargs='+', default=[None])
     parser.add_argument('--MPI_PROCESS_COUNT', type=int, nargs='+', default=[None])
+    parser.add_argument('--NUM_GPU', type=int, nargs='+', default=[None])
     parser.add_argument('--cpu', action='store_true')
     return parser
 
